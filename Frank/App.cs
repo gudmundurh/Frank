ed42afd008f757d20e6cdcbc2570c0d17435ae1f
Gudmundur Hreidarsson <gudmundur.hreidarsson@gmail.com>
2009-07-23 15:20:09 +0200

    Frank is Sinatra .NET

---
+++ Frank/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Frank
+{
+    public class App : FrankApp
+    {
+        public override void Setup()
+        {
+            Get("/", r => "Hello world");
+            Get("/hello/{name}", r => "Hello there [somehow get name ;)]");
+        }
+    }
+
+    public abstract class FrankApp
+    {
+        private readonly RouteCollection _routeCollection = new RouteCollection();
+
+        public abstract void Setup();
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Implement also Post, Put and Delete
+        protected void Get(string url, Func<ViewContext, object> func)
+        {
+            var route = new Route(url, new FrankRouteHandler(func));
+            _routeCollection.Add(route);
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FrankRouteHandler : IRouteHandler
+    {
+        public FrankRouteHandler(Func<ViewContext, object> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IRouteHandler Members
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Frank/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Frank
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new App().Run();
+        }
+    }
+}
